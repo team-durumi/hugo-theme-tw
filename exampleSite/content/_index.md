07ce23f75c18d27260733b6e451dbad3b9f2dd4c
yeongtaek, Hong <mozodev@users.noreply.github.com>
2022-02-28 10:35:30 +0900

    tailwindcss 를 hugo pipe 에 흘려보냅니다.

--- exampleSite/content/_index.md
+++ exampleSite/content/_index.md
@@ -3,3 +3,7 @@ title: test
 ---
 
 test
+
+{{<rawhtml>}}
+<h1 class="text-white">test</h1>
+{{</rawhtml>}}
---
+++ exampleSite/tailwindcss.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    '../layouts/**/*.html',
+    '../assets/index.js',
+    'content/**/*.md',
+    './hugo_stats.json'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
